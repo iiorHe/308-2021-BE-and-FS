ba118d122e330df0e6924bb009ce2a28efe12064
iiorHe <georgeozharenkov@gmail.com>
2021-09-11 07:00:41 +0300

    Initial Commit, Individual work 1

---
+++ resources/views/myFirstPage.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Backend & FullStack Individual Task 1</title>
+    </head>
+    <body>
+        <h1>Heorhii Ozharenkov</h1>
+        <h2>Group 308</h2>
+    </body>
+</html>
