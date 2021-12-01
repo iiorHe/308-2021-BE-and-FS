1b088a25a2d06712b786202b65d11dfb99dc1ce5
iiorHe <georgeozharenkov@gmail.com>
2021-12-01 07:24:58 +0200

    Ind-4

---
+++ database/migrations/2021_12_01_052137_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('year');
+            $table->string('title',36);
+            $table->string('genre',28);
+            $table->string('devs',32);
+            $table->string('engine',16);
+            $table->string('platform',18);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
